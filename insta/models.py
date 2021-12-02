d5bc0022639cc9e1aa23d8b9a52cd00877c85ab3
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-02 18:27:44 +0300

    add the models class

--- insta/models.py
+++ insta/models.py
@@ -2,4 +2,5 @@ from django.db import models
 import datetime as dt
 from django.contrib.auth.models import User
 from django.db.models.fields import related
+from cloudinary.models import CloudinaryField
 # Create your models here.
