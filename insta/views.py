11768a7231f95c9de3ae4d87bc0daade64283f11
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-02 18:19:53 +0300

    add imports

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+
 
 # Create your views here.
