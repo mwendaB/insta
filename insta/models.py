3c4a61ca80ce846dd7a059332953dabf16a57a7b
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-02 18:29:25 +0300

    add the class for posts

--- insta/models.py
+++ insta/models.py
@@ -3,4 +3,16 @@ import datetime as dt
 from django.contrib.auth.models import User
 from django.db.models.fields import related
 from cloudinary.models import CloudinaryField
+
 # Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name='images')
+    image=CloudinaryField('image')
+    image_name = models.CharField(max_length=50)
+    image_caption = models.TextField()
+    posted_on = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
+
