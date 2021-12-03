d578081dce6f422e565c530a67c21f396413a040
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-03 17:04:07 +0300

    add the url section

--- insta/urls.py
+++ insta/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('profile/', views.profile, name='profile'),
+    path('profile/update/', views.update_profile, name='update.profile'),
 ]
