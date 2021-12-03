dd6498570ae065776149a19b638c2f2330c0e276
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-03 17:01:18 +0300

    finish writting the profile class

--- insta/models.py
+++ insta/models.py
@@ -55,3 +55,21 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, blank=True, null=True)
     contact = models.CharField(max_length=50, blank=True, null=True)
 
+    def update(self):
+        self.save()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profile_by_user(cls, user):
+        profile = cls.objects.filter(user=user)
+        return profile
+
+    def __str__(self):
+        return self.user.username
+
+
