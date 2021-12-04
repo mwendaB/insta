8ab9709cb00cd2dd7a9397f19ff9b57cb54aca7a
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-04 16:23:28 +0300

    add the forms and the email .py file

--- insta/views.py
+++ insta/views.py
@@ -1,178 +1,113 @@
 from django.shortcuts import render, redirect
+from .forms import ProfileForm,ImageForm, CommentForm
 from django.contrib.auth.decorators import login_required
-from .models import *
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
+from django.contrib.auth.models import User
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Image, Profile,Comments,Likes
+from vote.managers import VotableManager
 
-
-# Create your views here.
-@login_required(login_url='/accounts/login/')
-def home(request):
-    posts = Post.objects.all()
-    return render(request, 'home.html', {'posts': posts})
+votes=VotableManager()
 
 @login_required(login_url='/accounts/login/')
-def profile(request):
+def index(request):
     current_user = request.user
-    # get images for the current logged in user
-    posts = Post.objects.filter(user_id=current_user.id)
-    # get the profile of the current logged in user
-    profile = Profile.objects.filter(user_id=current_user.id).first()
-    return render(request, 'profile.html', {"posts": posts, "profile": profile})
-
+    posts = Image.get_all_images()
+    comments = Comments.objects.all()
+    profile = Profile.get_all_profiles()
+    
+    
+    return render(request, 'insta/index.html', locals())
+    
 @login_required(login_url='/accounts/login/')
-def new_post(request):
-    if request.method == 'POST':
-        image_name = request.POST['image_name']
-        image_caption = request.POST['image_caption']
-        image_file = request.FILES['image_file']
-        image_file = cloudinary.uploader.upload(image_file)
-        image_url = image_file['url']
-        # image_public_id = image_file['public_id']
-        image = Post(image_name=image_name, image_caption=image_caption, image=image_url,
-                      profile_id=request.POST['user_id'], user_id=request.POST['user_id'])
-        image.save_image()
-        return redirect('/', {'success': 'Image Uploaded Successfully'})
-    else:
-        return render(request, 'profile.html', {'danger': 'Image Upload Failed'})
-
-# update profile with first name,last name,username,email and upload profile image to cloudinary
-@login_required(login_url='/accounts/login/')
-def update_profile(request):
-    if request.method == 'POST':
-
+def add_image(request):
         current_user = request.user
-
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        username = request.POST['username']
-        email = request.POST['email']
-
-        bio = request.POST['bio']
-
-        profile_image = request.FILES['profile_pic']
-        profile_image = cloudinary.uploader.upload(profile_image)
-        profile_url = profile_image['url']
-
-        user = User.objects.get(id=current_user.id)
-
-        # check if user exists in profile table and if not create a new profile
-        if Profile.objects.filter(user_id=current_user.id).exists():
-            profile = Profile.objects.get(user_id=current_user.id)
-            profile.profile_photo = profile_image
-            profile.bio = bio
-            profile.save()
+        if request.method == 'POST':
+                form = ImageForm(request.POST, request.FILES)
+                if form.is_valid():
+                        add=form.save(commit=False)
+                        add.user = current_user
+                        add.save()
+                return redirect('index')
         else:
-            profile = Profile(user_id=current_user.id,
-                              profile_photo=profile_url, bio=bio)
-            profile.save_profile()
+                form = ImageForm()
+                return render(request,'insta/image.html', {"form":form})
 
-        user.first_name = first_name
-        user.last_name = last_name
-        user.username = username
-        user.email = email
-
-        user.save()
-
-        return redirect('/profile', {'success': 'Profile Updated Successfully'})
-
+@login_required(login_url='/accounts/login/')                
+def profile_info(request):
+        current_user = request.user
+        profile = Profile.objects.filter(user=current_user).first()
+        posts = request.user.image_set.all()
        
-    else:
-        return render(request, 'profile.html', {'danger': 'Profile Update Failed'})
+        return render(request, 'insta/profile.html', {"images": posts, "profile": profile})
+@login_required(login_url='/accounts/login/') 
+def profile_update(request):
+         current_user = request.user
+         if request.method == 'POST':
+                form = ProfileForm(request.POST, request.FILES)
+                if form.is_valid():
+                        add=form.save(commit=False)
+                        add.user = current_user
+                        add.save()
+                return redirect('profile')
+         else:
+                form = ProfileForm()
+         return render(request,'insta/profile_update.html',{"form":form})
 
-
-# like image
-@login_required(login_url='/accounts/login/')
-def like_image(request, id):
-    likes = Like.objects.filter(post_id=id).first()
-    # check if the user has already liked the image
-    if Like.objects.filter(post_id=id, user_id=request.user.id).exists():
-        # unlike the image
-        likes.delete()
-        # reduce the number of likes by 1 for the image
-        post = Post.objects.get(id=id)
-        # check if the image like_count is equal to 0
-        if post.likes == 0:
-            post.likes = 0
-            post.save()
+@login_required(login_url='/accounts/login/') 
+def comment(request,image_id):
+        current_user=request.user
+        image = Image.objects.get(id=image_id)
+        profile_owner = User.objects.get(username=current_user.username)
+        comments = Comments.objects.all()
+        
+        if request.method == 'POST':
+                form = CommentForm(request.POST, request.FILES)
+                if form.is_valid():
+                        comment = form.save(commit=False)
+                        comment.image = image
+                        comment.user = request.user
+                        comment.save()
+            
+                       
+                return redirect('index')
         else:
-            post.likes -= 1
-            post.save()
-        return redirect('/')
-    else:
-        likes = Like(post_id=id, user_id=request.user.id)
-        likes.save()
-        # increase the number of likes by 1 for the image
-        post = Post.objects.get(id=id)
-        post.likes = post.likes + 1
-        post.save()
-        return redirect('/')
+                form = CommentForm()
+        return render(request, 'insta/comment.html',locals())
 
 
-# single image page with comments
 @login_required(login_url='/accounts/login/')
-def view_post(request, id):
-    post = Post.objects.get(id=id)
-    # get related images to the image that is being viewed by the user and order them by the date they were created
-    related_posts = Post.objects.filter(
-        user_id=post.user_id)
-    title = post.image_name
-    # check if image exists
-    if Post.objects.filter(id=id).exists():
-        # get all the comments for the image
-        comments = Comment.objects.filter(post_id=id)
-        return render(request, 'picture.html', {'post': post, 'comments': comments, 'posts': related_posts, 'title': title})
+def search_results(request):
+    if 'username' in request.GET and request.GET["username"]:
+        search_term = request.GET.get("username")
+        searched_users = User.objects.filter(username__icontains = search_term)
+        message = f"{search_term}"
+        profile_pic = User.objects.all()
+        return render(request, 'insta/search.html', {'message':message, 'results':searched_users, 'profile_pic':profile_pic})
     else:
-        return redirect('/')
+        message = "You haven't searched for any term"
+        return render(request, 'insta/search.html', {'message':message})
 
+def follow(request, user_id):
+    other_user = User.objects.get(id = user_id)
+    follow = Follow.objects.add_follower(request.user, other_user)
 
-# save comment
-@login_required(login_url='/accounts/login/')
-def add_comment(request):
-    if request.method == 'POST':
-        comment = request.POST['comment']
-        post_id = request.POST['post_id']
-        post = Post.objects.get(id=post_id)
+    return redirect('index')
+
+def unfollow(request, user_id):
+    other_user = User.objects.get(id = user_id)
+    follow = Follow.objects.remove_follower(request.user, other_user)
+
+    return redirect('index')
+
+@login_required(login_url='/accounts/register/')
+def like_images(request, id):
+        image = Image.get_one_image(id)
         user = request.user
-        comment = Comment(comment=comment, post_id=post_id, user_id=user.id)
-        comment.save_comment()
-        # increase the number of comments by 1 for the image
-        post.comments = post.comments + 1
-        post.save()
-        return redirect('/picture/' + str(post_id))
-    else:
-        return redirect('/')
+        user_id = user.id
 
+        if user.is_authenticated:
+                uplike = image.votes.up(user_id)
+                image.like_add = image.votes.count()
+                image.save()
 
-# user profile page with images
-@login_required(login_url='/accounts/login/')
-def user_profile(request, id):
-    # check if user exists
-    if User.objects.filter(id=id).exists():
-        # get the user
-        user = User.objects.get(id=id)
-        # get all the images for the user
-        posts = Post.objects.filter(user_id=id)
-        # get the profile of the user
-        profile = Profile.objects.filter(user_id=id).first()
-        return render(request, 'user-profile.html', {'posts': posts, 'profile': profile, 'user': user})
-    else:
-        return redirect('/')
-
-
-# search for images
-@login_required(login_url='/accounts/login/')
-def search_posts(request):
-    if 'search' in request.GET and request.GET['search']:
-        search_term = request.GET.get('search').lower()
-        posts = Post.search_by_image_name(search_term)
-        message = f'{search_term}'
-        title = message
-
-        return render(request, 'search.html', {'success': message, 'posts': posts})
-    else:
-        message = 'You havent searched for any term'
-        return render(request, 'search.html', {'danger': message})
-
-
+        return redirect('index')
